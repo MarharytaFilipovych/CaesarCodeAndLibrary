a5ebf12b21ad6b83b621f8a69ae3747d7975832b
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-06-12 20:40:39 +0300

    Library

---
+++ Ceasar.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+extern "C"
+{
+    _declspec(dllexport) char* Encrypt(char* rawText, int key)
+    {
+        int length = strlen(rawText);
+        char* encodedText = new char[length + 1];
+
+        for (int i = 0; i < length; i++)
+        {
+            if (!isalpha(rawText[i]))
+            {
+                encodedText[i] = rawText[i];
+            }
+            else if (isupper(rawText[i]))
+            {
+                encodedText[i] = ((rawText[i] - 65 + key) % 26 + 26) % 26 + 65;
+            }
+            else if (!isupper(rawText[i]))
+            {
+                encodedText[i] = ((rawText[i] - 97 + key) % 26 + 26) % 26 + 97;
+            }
+
+        }
+        encodedText[length] = '\0';
+        return encodedText;
+    }
+
+    _declspec(dllexport) char* Decrypt(char* encryptedText, int key)
+    {
+        char* text = Encrypt(encryptedText, -key);
+        delete[] encryptedText;
+        return text;
+    }
+}
