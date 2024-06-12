9698e981cbb5756f157ed23ed30bc00562b3a85e
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-06-12 20:41:22 +0300

    main file code

---
+++ main.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include <iostream>
+#include <windows.h>
+int main() {
+	typedef char* (*encrypt_ptr_t)(char*, int);
+	typedef char* (*decrypt_ptr_t)(char*, int);
+
+	HINSTANCE handle = LoadLibrary(TEXT("caesar.dll"));
+	if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
+	{
+		cout << "Lib not found" << endl;
+		return -1;
+	}
+	encrypt_ptr_t encrypt_ptr = (encrypt_ptr_t)GetProcAddress(handle, "Encrypt");
+	if (encrypt_ptr == nullptr)
+	{
+		cout << "Function not found" << endl;
+		return 1;
+	}
+
+	decrypt_ptr_t decrypt_ptr = (decrypt_ptr_t)GetProcAddress(handle, "Decrypt");
+	if (decrypt_ptr == nullptr)
+	{
+		cout << "Function not found" << endl;
+		return 1;
+	}
+	int key = 22;
+	char phrase[] = "Today is a good day, but not tomorrow!";
+	cout << phrase << endl;
+	cout << "Encryption by key: " << key << " " << encrypt_ptr(phrase, key) << endl;
+	char* encryptedText = encrypt_ptr(phrase, key);
+	cout << "Decryption by key: " << key << " " << decrypt_ptr(encryptedText, key) << endl;
+
+	FreeLibrary(handle);
+}
+
